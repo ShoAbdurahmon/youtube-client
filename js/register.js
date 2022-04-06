38ca5275260d796b176280d9933380884933b410
ShoAbdurahmon <91741205+ShoAbdurahmon@users.noreply.github.com>
2022-04-06 14:33:29 +0500

    d

--- js/register.js
+++ js/register.js
@@ -6,6 +6,7 @@ form.onsubmit = async function (event) {
     let username = usernameInput.value
     let password = passwordInput.value
     let file = uploadInput.files[0]
+    console.log(file)
 
     let response = await request('/register', 'POST', { username, password, file })
     console.log(response)
