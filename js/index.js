2acb78476745da97b9576c225bc994cfc827a2b0
ShoAbdurahmon <91741205+ShoAbdurahmon@users.noreply.github.com>
2022-04-06 15:57:02 +0500

    a

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
 async function renderUsers(){
     let response = await request('/info', 'GET')
-    console.log(response.users)
+    users = response.users
+
+    users.innerHTML = null
+    users.innerHTML =  `<h1>YouTube Members</h1>
+                        <li class="channel active" data_id="main">
+                            <a href="#">
+                                <svg viewbox="0 0 24 24" focusable="false" style="pointer-events: none; display: block; width: 30px; height: 30px;"><g><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8" class="style-scope yt-icon"></path></g></svg>
+                                <span>Home</span>
+                            </a>
+                        </li>`
+                    
+    for(let i of users){
+        users.innerHTML += `<li class="channel" data-id="1">
+                                <a href="#">
+                                    <img src="${i.fileName}" alt="channel-icon" width="30px" height="30px">
+                                    <span>${i.username}</span>
+                                </a>
+                            </li>`
+    }
 }
 renderUsers()
